ef5237f6113c3dec01cbd7aab8b130b396c45d15
MathewMerrick <dev@mathewmerrick.com>
2015-12-05 16:48:22 -0800

    MVVM, Game ViewModel
    Last commit didn't include this.

---
+++ pongit/ViewModel/GameViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using pongit.Model;
+
+namespace pongit.ViewModel{
+
+    class GameViewModel {
+
+        private Ball _ball;
+        private Paddle _leftPaddle;
+        private Paddle _rightPaddle;
+        private Score _score;
+
+        public GameViewModel() {
+            _ball = new Ball {x = 525, y = 325};
+                //Left X: 0     Right X: 1050
+                //Min Y: 0      Max Y: 650
+
+            _leftPaddle = new Paddle {y = 275}; 
+                //0 is bottom, 600 is top
+
+            _rightPaddle = new Paddle {y = 275}; 
+                //0 is bottom, 600 is top
+
+            _score = new Score {left = 0, right = 0};
+                //Set both scores to zero
+        }
+
+        public Ball ball {
+            get { return _ball; }
+            set { _ball = value; }
+        }
+
+        public Paddle leftPaddle {
+            get { return _leftPaddle; }
+            set { _leftPaddle = value; }
+        }
+
+        public Paddle rightPaddle {
+            get { return _rightPaddle; }
+            set { _rightPaddle = value; }
+        }
+
+        public Score score {
+            get { return _score; }
+            set { _score = value; }
+        }
+
+    }
+}
